09f66460613684a40e9ea4a6794fb853dca56daa
xlk <1148038404@qq.com>
2024-03-18 11:20:56 +0800

    chore:  init program config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        console.log('你好啊， 李银河');
+
+        return this.appService.getHello();
+    }
 }
