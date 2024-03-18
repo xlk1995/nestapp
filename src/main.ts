96013852c4c1e4a8d033c703c3fc6070899491e5
xlk <1148038404@qq.com>
2024-03-18 14:36:41 +0800

    feat: 修改http驱动为fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+        // 允许跨域访问
+        cors: true,
+        // 只打印error 和warn
+        logger: ['error', 'warn'],
+    });
+    // 设置访问前缀
+    app.setGlobalPrefix('api');
+    // 启动后输出
+    await app.listen(3100, () => {
+        console.log('api: http://localhost:3100');
+    });
 }
 bootstrap();
