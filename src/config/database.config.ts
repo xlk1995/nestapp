caa92129050c5c6c89f24c39255b2e97ebdac4e3
xlk <1148038404@qq.com>
2024-03-19 11:18:37 +0800

    feat: 添加一些typeorm辅助配置函数

---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { resolve } from 'path';
+
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const database = (): TypeOrmModuleOptions => ({
+    type: 'better-sqlite3',
+    database: resolve(__dirname, '../../back/database2.db'),
+    // 因为我们目前没有涉及到数据迁移的命令编写，所以必须在启动数据库时自动根据加载的模型(Entity)来同步数据表到数据库
+    synchronize: true,
+    // 这样我们就不需要把每个模块的Entity逐个定死地添加到配置中的entities数组中了，因为你可以在每个模块中使用TypeOrmModule.forFeature来动态的加入Entity
+    autoLoadEntities: true,
+});
--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,7 +1,43 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module, Provider, Type } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions, getDataSourceToken } from '@nestjs/typeorm';
 
-@Module({
-    controllers: [],
-    providers: [],
-})
-export class DatabaseModule {}
+import { DataSource, ObjectType } from 'typeorm';
+
+import { CUSTOM_REPOSITORY_METADATA } from './constants';
+
+@Module({})
+export class DatabaseModule {
+    static forRoot(configRegister: () => TypeOrmModuleOptions): DynamicModule {
+        return {
+            global: true,
+            module: DatabaseModule,
+            imports: [TypeOrmModule.forRoot(configRegister())],
+        };
+    }
+
+    static forRepository<T extends Type<any>>(
+        repositories: T[],
+        dataSourceName?: string,
+    ): DynamicModule {
+        const providers: Provider[] = [];
+        for (const Repo of repositories) {
+            const entity = Reflect.getMetadata(CUSTOM_REPOSITORY_METADATA, Repo);
+            if (!entity) {
+                continue;
+            }
+            providers.push({
+                inject: [getDataSourceToken(dataSourceName)],
+                provide: Repo,
+                useFactory: (dataSource: DataSource): InstanceType<typeof Repo> => {
+                    const base = dataSource.getRepository<ObjectType<any>>(entity);
+                    return new Repo(base.target, base.manager, base.queryRunner);
+                },
+            });
+        }
+        return {
+            exports: providers,
+            module: DatabaseModule,
+            providers,
+        };
+    }
+}
